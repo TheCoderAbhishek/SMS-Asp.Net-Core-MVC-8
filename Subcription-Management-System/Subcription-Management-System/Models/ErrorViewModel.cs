a7766d1ab2f57d290b8802b885e0bd3ec74ed5dc
AbhishekHPatil <abhishek.hpatil63@gmail.com>
2024-02-28 13:22:38 +0530

    Start creating project

---
+++ Subcription-Management-System/Subcription-Management-System/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+namespace Subcription_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
